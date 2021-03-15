e17be4f54e5129ca23955c9f6a1f01e970265dfb
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2021-03-15 18:46:26 +1300

    set up profile header

--- src/components/profile/header.js
+++ src/components/profile/header.js
@@ -1,7 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import useUser from '../../hooks/use-user';
 
-export default function Header () {
+export default function Header ({
+  photoCount,
+  followerCount,
+  setFollowerCount,
+  profile,
+  username
+}) {
+  const { user } = useUser();
+  const [isFollowingProfile, setIsFollowingProfile] = useState(false);
+  const activeBtnFollow = user.username && user.username !== username;  
+
   return (
-    <p>I am the Header of the profile</p>
+    <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+      <div className="container flex justify-center">
+        <img
+          className="rounded-full h-40 flex"
+          alt={`${username} profile picture`}
+          src={`/images/avatars/${username}.jpg`}
+        />
+      </div>
+      <div className="flex items-center justify-center flex-col col-span-2">
+        <div className="container flex items-center">
+          <p className="text-2xl mr-4">{username}</p>
+          {activeBtnFollow && (
+            <button
+              className="bg-blue-500 font-bold text-sm rounded text-white w-20 h-8"
+              type="button"
+              onClick={() => console.log('I am a button')}
+            >
+              {isFollowingProfile ? 'Following' : 'Follow'}
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
   )
 }
